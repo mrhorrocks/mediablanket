e04eb7f34e308a48b8b6f8c95ec697330f63fcdd
Mark Horrocks <mr.horrocks@gmail.com>
2024-10-05 17:43:03 +0100

    router testing

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>THE HEADER</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import TheHeader from '@/components/TheHeader.vue'
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
+    <TheHeader />
     <TheWelcome />
   </main>
 </template>
